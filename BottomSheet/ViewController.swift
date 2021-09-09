5205ff3a41e0b1155f2cb9f3b950914d1b54132d
Nishant Taneja <nishant.taneja2701@gmail.com>
2021-09-10 01:20:38 +0530

    initial commit

--- BottomSheet/ViewController.swift
+++ BottomSheet/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#function)
     }
 
-
 }
-
